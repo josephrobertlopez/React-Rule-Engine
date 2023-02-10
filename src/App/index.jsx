a2241ea597e73a46165e2b55cbae46f4c8343972
josephrobertlopez <josephrobertlopez@gmail.com>
2023-02-10 11:48:27 -0600

    Refactor Form
    Form saves a rule to local storage
    
    Form moved to its own file

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,55 +1,8 @@
 import * as React from 'react';
-import { Ruleset } from "../components/Ruleset";
-import {Rule} from "../components/Rule";
-import { render } from 'react-dom';
-
-const RuleField = ({ onChange }) => (
-  <div>
-    <input type="text" placeholder="condition"  onChange={(event) => {onChange({type:'condition',payload:event.target.value})}} />
-    <input type="text" placeholder="consequence" onChange={(event) => {onChange({type:'consequence',payload:event.target.value})}} />
-    <input type="text" placeholder="alternative" onChange={(event) => {onChange({type:'alternative',payload:event.target.value})}}/>
-  </div>
-);
-
-const InputField = ({ onChange }) => (
-  <div>
-    <input type="text" placeholder="input variable"  onChange={(event) => {onChange({type:'inputVariable',payload:event.target.value})}} />
-    <input type="text" placeholder="input value"  onChange={(event) => {onChange({type:'inputValue',payload:event.target.value})}}/>
-  </div>
-);
-const Form = ({rule,input,onRuleChange, onInputChange}) => {
-   const [formData,setFormData] = React.useState(null);
-   const handleSubmit = (event) => {
-    event.preventDefault();
-    let tmpInput = {};
-    tmpInput[input.inputVariable] = input.inputValue;
-    setFormData({condition:rule.condition,consequence:rule.consequence,alternative:rule.alternative});
-    localStorage.setItem('rule',JSON.stringify(formData));
-  };
-   return(
-    <form onSubmit={handleSubmit}>
-       <div>
-        <RuleField
-          onChange={onRuleChange}
-          condition={rule.condition}
-          consequence={rule.consequence}
-          alternative={rule.alternative}
-        />
-        <InputField
-          onChange={onInputChange}
-          inputVariable={input.inputVariable}
-          inputValue={input.inputValue}
-        />
-      <button type="submit">Submit</button>
-      </div>
-    </form>
-     
-  )
-}
+import {Form} from "../components/Form";
 const App = () => {
 
   const initialRuleState = {condition:'',consequence:'',alternative:''};  
-  const initialInputState = {inputVariable:'',inputValue:''}
 
   const ruleReducer = (rule,action) => {
     switch(action.type){
@@ -66,24 +19,15 @@ const App = () => {
         throw new Error(`Error with Rule Reducer: action.type=${action.type} action.payload=${action.payload}`);
     }
   }
-  const inputReducer = (input,action) => {
-    switch(action.type){
-      case 'inputVariable':
-        return {inputVariable:action.payload,inputValue:input.inputValue};
-      case 'inputValue':
-        return {inputVariable:input.inputVariable,inputValue:action.payload};
-      default:
-        throw new Error(`Error with Input Reducer: action.type=${action.type} action.payload=${action.payload}`);
-    }
-  }
   const [rule,dispatchRule] = React.useReducer(ruleReducer,initialRuleState);
-  const [input,dispatchInput] = React.useReducer(inputReducer,initialInputState);
+  const [formData,setFormData] = React.useState(null);
+
   return(
     <Form
       rule={rule}
-      input={input}
+      formData={formData}
       onRuleChange={dispatchRule}
-      onInputChange={dispatchInput}
+      onFormSubmit={setFormData}
     />
   )
  
--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,80 +1,32 @@
 import * as React from 'react';
-
-const RuleInput = ({ onChange, index }) => (
+const RuleField = ({ onChange }) => (
   <div>
-    <input type="text" placeholder="condition" onChange={e => onChange(e, 'condition', index)} />
-    <input type="text" placeholder="consequence" onChange={e => onChange(e, 'consequence', index)} />
-    <input type="text" placeholder="alternative" onChange={e => onChange(e, 'alternative', index)} />
+    <input type="text" placeholder="condition"  onChange={(event) => {onChange({type:'condition',payload:event.target.value})}} />
+    <input type="text" placeholder="consequence" onChange={(event) => {onChange({type:'consequence',payload:event.target.value})}} />
+    <input type="text" placeholder="alternative" onChange={(event) => {onChange({type:'alternative',payload:event.target.value})}}/>
   </div>
 );
 
-const Input = ({ onChange, index }) => (
-  <div>
-    <input type="text" placeholder="input" onChange={e => onChange(e, 'input', index)} />
-  </div>
-);
-
-const Form = ({ rules, inputs, onRuleChange, onInputChange, onSubmit }) => {
-  const [ruleInputs, setRuleInputs] = React.useState(rules);
-  const [inputValues, setInputValues] = React.useState(inputs);
-
-  const handleRuleChange = (e, field, index) => {
-    const newRuleInputs = [...ruleInputs];
-    newRuleInputs[index][field] = e.target.value;
-    setRuleInputs(newRuleInputs);
-    onRuleChange(newRuleInputs);
+const Form = ({rule,formData, onRuleChange, onFormSubmit}) => {
+   const handleSubmit = (event) => {
+    event.preventDefault();
+    onFormSubmit({condition:rule.condition,consequence:rule.consequence,alternative:rule.alternative});
+    localStorage.setItem('rule',JSON.stringify({condition:rule.condition,consequence:rule.consequence,alternative:rule.alternative}));
   };
-
-  const handleInputChange = (e, field, index) => {
-    const newInputValues = [...inputValues];
-    newInputValues[index][field] = e.target.value;
-    setInputValues(newInputValues);
-    onInputChange(newInputValues);
-  };
-
-  const handleSubmit = e => {
-    e.preventDefault();
-    onSubmit(ruleInputs, inputValues);
-  };
-
-  const addRuleInput = () => {
-    setRuleInputs([...ruleInputs, { condition: '', consequence: '', alternative: '' }]);
-  };
-
-  const addInput = () => {
-    setInputValues([...inputValues, { input: '' }]);
-  };
-
-  return (
+   return(
     <form onSubmit={handleSubmit}>
-      <div>
+       <div>
         <h3>Rules</h3>
-        {ruleInputs.map((ruleInput, index) => (
-          <RuleInput key={index} index={index} onChange={handleRuleChange} />
-        ))}
-        <button type="button" onClick={addRuleInput}>
-          +
-        </button>
-      </div>
-      <div>
-        <h3>Inputs</h3>
-        {inputValues.map((input, index) => (
-          <Input key={index} index={index} onChange={handleInputChange} />
-        ))}
-        <button type="button" onClick={addInput}>
-          +
-        </button>
-      </div>
+        <RuleField
+          onChange={onRuleChange}
+          condition={rule.condition}
+          consequence={rule.consequence}
+          alternative={rule.alternative}
+        />
       <button type="submit">Submit</button>
+      </div>
     </form>
-  );
+     
+  )
 };
-
-const Results = ({ results }) => (
-  <div>
-    <h3>Results</h3>
-    <pre>{JSON.stringify(results, null, 2)}</pre>
-  </div>
-);
-
-export { Form, Results };
+export {Form};
