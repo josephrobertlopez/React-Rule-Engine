b39d149d89c92b5d4227ce69b30c2304139fe95f
josephrobertlopez <josephrobertlopez@gmail.com>
2023-02-10 13:05:08 -0600

    Update server.cjs

--- server.cjs
+++ server.cjs
@@ -1,5 +1,8 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
+app.use(bodyParser.json());
 
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
