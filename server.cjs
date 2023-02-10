043bd992389aa9a3f2cd26a5c60d348d405dc02f
josephrobertlopez <josephrobertlopez@gmail.com>
2023-02-10 12:55:27 -0600

    Create server.cjs

---
+++ server.cjs
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+app.post('/rule', (req, res) => {
+  console.log('Received data:', req.body);
+  res.sendStatus(200);
+});
+
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
