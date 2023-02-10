6e19fe57578e470b2fafe6319cc692bd3cc2992d
josephrobertlopez <josephrobertlopez@gmail.com>
2023-02-10 13:40:57 -0600

    Update server.cjs

--- server.cjs
+++ server.cjs
@@ -1,7 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
+app.use(cors({origin:'http://localhost:5173'}))
 app.use(bodyParser.json());
 
 app.use(function(req, res, next) {
